50051b090e83a5a945fcab74854b7781e8a7a4ac
yebingyong <935541526@qq.com>
2020-12-13 19:45:26 +0800

    1

--- vinsaoma/vincode-platecode11.30/www/PlateCode.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var PlateCode = {
-	scan: function (success, error) {
-		exec(success, error, 'PlateCode', 'scan', []);
-	},
-};
-
-module.exports = PlateCode;

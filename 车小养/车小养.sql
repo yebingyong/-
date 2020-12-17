ec9dfa29c3e2e8a196c0db114f71ce037d4010b9
yeby <yeby@mancando.cn>
2020-12-17 16:33:43 +0800

    1

--- 车小养/车小养.sql
+++ 车小养/车小养.sql
@@ -67,6 +67,8 @@ CREATE TABLE `store_pic` (
   KEY `store_id` (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='门店图片';
 
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('store_pic',0);
+
 CREATE TABLE `store_tag` (
   `id` bigint(20) NOT NULL COMMENT 'ID',
   `tenant_id` int(11) NOT NULL COMMENT '租户ID',
@@ -76,6 +78,8 @@ CREATE TABLE `store_tag` (
   KEY `tenant_id` (`tenant_id`),
   KEY `store_id` (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='门店标签';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('store_tag',0);
 
 CREATE TABLE `reservation_service_category` (
   `id` bigint(20) NOT NULL COMMENT '类别ID',
@@ -89,6 +93,8 @@ CREATE TABLE `reservation_service_category` (
   KEY `store_id` (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='预约项目分类';
 
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('reservation_service_category',0);
+
 CREATE TABLE `shopping_mall_product_category` (
   `id` bigint(20) NOT NULL COMMENT '类别ID',
   `tenant_id` int(11) NOT NULL COMMENT '租户ID',
@@ -100,5 +106,106 @@ CREATE TABLE `shopping_mall_product_category` (
   KEY `tenant_id` (`tenant_id`),
   KEY `store_id` (`store_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='商城商品分类';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('shopping_mall_product_category',0);
+
+CREATE TABLE `shopping_mall_product` (
+  `id` bigint(20) NOT NULL,
+  `tenant_id` int(11) NOT NULL COMMENT '租户ID',
+  `store_id` bigint(20) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '名称',
+  `shopping_mall_product_category_id` bigint(20) NOT NULL,
+  `sale_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '销售价(元)',
+  `description` varchar(255) COLLATE utf8_bin DEFAULT '' COMMENT '描述',
+  `create_time` datetime NOT NULL COMMENT '添加时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`) USING BTREE,
+  KEY `store_id` (`store_id`) USING BTREE,
+  KEY `name` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='商场商品';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('shopping_mall_product',0);
+
+CREATE TABLE `shopping_mall_product` (
+  `id` bigint(20) NOT NULL,
+  `tenant_id` int(11) NOT NULL COMMENT '租户ID',
+  `store_id` bigint(20) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '名称',
+  `shopping_mall_product_category_id` bigint(20) NOT NULL,
+  `sale_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '销售价(元)',
+  `description` varchar(255) COLLATE utf8_bin DEFAULT '' COMMENT '描述',
+  `create_time` datetime NOT NULL COMMENT '添加时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`) USING BTREE,
+  KEY `store_id` (`store_id`) USING BTREE,
+  KEY `name` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='商城商品';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('shopping_mall_product',0);
+
+CREATE TABLE `shopping_mall_product_item` (
+  `id` bigint(20) NOT NULL,
+  `tenant_id` int(11) NOT NULL COMMENT '租户ID',
+  `store_id` bigint(20) NOT NULL,
+  `shopping_mall_product_id` bigint(20) NOT NULL COMMENT '商城商品ID',
+  `product_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `unit_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '单价',
+  `quantity` int(11) DEFAULT NULL COMMENT '数量',
+  `price_calculation_method` tinyint(4) NOT NULL DEFAULT '1' COMMENT '价格计算方式（1：视加注量，2：自定义）',
+  `create_time` datetime NOT NULL COMMENT '添加时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`) USING BTREE,
+  KEY `store_id` (`store_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='商城商品单行';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('shopping_mall_product_item',0);
+
+CREATE TABLE `shopping_mall_product_item_pic` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `tenant_id` int(11) NOT NULL COMMENT '租户ID',
+  `store_id` bigint(20) NOT NULL,
+  `item_id` bigint(20) NOT NULL COMMENT '商城商品单行ID',
+  `pic` varchar(100) NOT NULL COMMENT '图片地址',
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`),
+  KEY `store_id` (`store_id`),
+  KEY `item_id` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商城商品单行图片';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('shopping_mall_product_item_pic',0);
+
+CREATE TABLE `hours_price` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `tenant_id` int(11) NOT NULL COMMENT '租户ID',
+  `store_id` bigint(20) NOT NULL COMMENT '门店ID',
+  `hours_price` decimal(8,2) NOT NULL COMMENT '工时单价(元)',
+  `create_time` datetime NOT NULL COMMENT '添加时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`) USING BTREE,
+  KEY `store_id` (`store_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='工时单价';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('hours_price',0);
+
+CREATE TABLE `hours_price_log` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `tenant_id` int(11) NOT NULL COMMENT '租户ID',
+  `store_id` bigint(20) NOT NULL COMMENT '门店ID',
+  `last_hours_price` decimal(8,2) NOT NULL COMMENT '上次工时单价(元)',
+  `hours_price` decimal(8,2) NOT NULL COMMENT '工时单价(元)',
+  `user_id` int(11) NOT NULL COMMENT '操作人ID',
+  `create_time` datetime NOT NULL COMMENT '添加时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `tenant_id` (`tenant_id`) USING BTREE,
+  KEY `store_id` (`store_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='工时单价日志';
+
+INSERT INTO `sequence` (`name`,`nextid`) VALUES ('hours_price_log',0);
+
 
 
